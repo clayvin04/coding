082242d997d0c62babe451c99e3bfc2e72046bf3
clayvin04 <clayvin04@gmail.com>
2024-02-03 18:56:00 +0100

    achine coffee

--- ko/codin.py
+++ ko/codin.py
@@ -1,46 +1,151 @@
-##########randi kfx da5l gamil ,name phone#########
+#####achine_coffee
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
 
 
-def add_user(name, gmail, phone, dec):
-        dec[name] = {"gmail":gmail, "phone": phone}
+game_end = True
 
 
-def delete_user(name, dec):
-    if name in dec:
-        dec.clear["name"]
+def espresso(quarters, dimes, nickles, pennies, MENU):
+    
+    total =  0.25*quarters + 0.1 * dimes + 0.05*nickles + 0.01 * pennies
+    if total > MENU["espresso"]["cost"]:
+        if water > MENU["espresso"]["ingredients"]["water"]:
+            if coffee  > MENU["espresso"]["ingredients"]["coffee"]:
+                return water - MENU["espresso"]["ingredients"]["water"]
+                return coffee  - MENU["espresso"]["ingredients"]["coffee"]
+                sum = total - MENU["cappuccino"]["cost"]
+                print(f"Here is {sum} in change.")
+                print(f"Here is your espresso ☕️. Enjoy!")
+            
+            else:
+                print("Sorry there is not enough coffee")
+                game_end = False
+        
+        
+        
+        
+        else:
+            print("Sorry there is not enough water")
+            game_end = False
+    
     else:
-        print("your data not found ")
+        print("I don't have enough money sir")
+        game_end = False
 
-def update_user(update, dec, name):
-    change = input(f"update your old  {update} : ").lower()
-    if name in dec:
-        dec[name][update] = change
+
+
+
+def latte(quarters, dimes, nickles, pennies, MENU):
+    total =  0.25*quarters + 0.1 * dimes + 0.05*nickles + 0.01 * pennies
+    if total > MENU["latte"]["cost"]:
+        if water > MENU["latte"]["ingredients"]["water"]:
+            if coffee  > MENU["latte"]["ingredients"]["coffee"]:
+                return water - MENU["latte"]["ingredients"]["water"]
+                return coffee  - MENU["latte"]["ingredients"]["coffee"]
+                sum = total - MENU["cappuccino"]["cost"]
+                print(f"Here is {sum} in change.")
+                print(f"Here is your latte ☕️. Enjoy!")
+            
+            else:
+                print("Sorry there is not enough coffee")
+                game_end = False
+        
+        
+        
+        
+        else:
+            print("Sorry there is not enough water")
+            game_end = False
+
+    
     else:
-        print('The user does not exist')
+        print("I don't have enough money sir")
+        game_end = False
+            
 
 
-def game():
-    dec = {}
-    service = input('what service do you want this time Sir. type "add" to add a new user, "clear" to delete your data, "update" to update your informations : ').lower()
-    name = input("ur name ").lower()
-    if service == 'add':
-        gmail = input("ur gmail ")
-        phone = int(input("ur phone "))
-        add_user(name, gmail, phone, dec)
-    elif service == 'clear':
-        delete_user(name, dec)
-        print("your name was deeted successfuly")
+def cappuccino(quarters, dimes, nickles, pennies, MENU):
+    total =  0.25*quarters + 0.1 * dimes + 0.05*nickles + 0.01 * pennies
+    if total > MENU["cappuccino"]["cost"]:
+        if water >= MENU["cappuccino"]["ingredients"]["water"]:
+            if coffee  >= MENU["cappuccino"]["ingredients"]["coffee"]:
+                return water - MENU["cappuccino"]["ingredients"]["water"]
+                return coffee  - MENU["cappuccino"]["ingredients"]["coffee"]
+                sum = total - MENU["cappuccino"]["cost"]
+                print(f"Here is {sum} in change.")
+                print(f"Here is your cappuccino ☕️. Enjoy!")
+            
+            else:
+                print("Sorry there is not enough coffee")
+                
+        
+        
+        
+        
+        else:
+            print("Sorry there is not enough water")
+            game_end = False
+    
     else:
-        print(dec)
-        update = input("what do u want to change. type 'gmail' to change the gmail, and 'phone' to change ur name ").lower()
-        update_user(update, name, dec)
-    print("thank you Sir ")
-
-
-game()
+        print("I don't have enough money sir")
+        return
 
 
 
+def Machine_coffee():
+    water = 300
+    milk= 200
+    coffee = 100
+    desire = input("What would you like? ('espresso'/'latte'/'cappuccino'):").lower()
+    if desire == 'espresso':
+        quarters = int(input("how many quarters?:") )
+        dimes = int(input("how many dimes?:")) 
+        nickles = int(input("how many nickles?:"))
+        pennies = int(input("how many pennies?:")) 
+        espresso(quarters, dimes, nickles, pennies, MENU, water, coffee) 
+    elif desire =='latte':
+        quarters = int(input("how many quarters?:") )
+        dimes = int(input("how many dimes?:")) 
+        nickles = int(input("how many nickles?:"))
+        pennies = int(input("how many pennies?:")) 
+        latte(quarters, dimes, nickles, pennies, MENU, water, coffee)
+    elif desire == 'cappuccino':
+        quarters = int(input("how many quarters?:") )
+        dimes = int(input("how many dimes?:")) 
+        nickles = int(input("how many nickles?:"))
+        pennies = int(input("how many pennies?:")) 
+        cappuccino(quarters, dimes, nickles, pennies, MENU,water, coffee)
+    elif desire == "d":
+        print(f"{water}ml")
+        print(f"{milk}ml")
+        print(f"{coffee}g")
 
+while game_end :
 
+    Machine_coffee()
+##################kfx n9as man water o milk o coffee ############################
 
